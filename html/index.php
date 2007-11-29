a5a210753df0ca613b026f04ddcf652432fb5170
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2007-11-29 10:40:31 +0000

    Updated date string, style

--- html/index.php
+++ html/index.php
@@ -266,7 +266,7 @@
             <a class="sharelink" href="http://www.facebook.com/share.php?u=<?= $url ?>">
                 <img class="sharelink" src="/nexml/html/img/facebook.gif"/>
             </a>   
-            <div class="date">
+            <div class="credit">
                 <?= date("r") ?> 
             </div>
         </div>        
