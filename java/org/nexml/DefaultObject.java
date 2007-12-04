b9820d28d7c535aed81421dc98e2155fb208d91a
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2007-12-04 01:15:27 +0000

    Added JavaDoc comments for DefaultObject

--- java/org/nexml/DefaultObject.java
+++ java/org/nexml/DefaultObject.java
@@ -1,5 +1,7 @@
 package org.nexml;
 
+// $Id$
+
 import org.xml.sax.Attributes;
 
 public class DefaultObject {
@@ -16,26 +18,50 @@ public class DefaultObject {
 		this.atts = myatts;
 	}
 	
+	/**
+	 * Gets the universal resource identifier of the object
+	 * @return a string of the URI, typically http://www.nexml.org/1.0
+	 */
 	public String getNamespaceURI () {
 		return this.namespaceURI;
 	}
 	
+	/**
+	 * Gets the local xml element name of the object
+	 * @return a local xml element name
+	 */
 	public String getLocalName () {
 		return this.localName;
 	}
 	
+	/**
+	 * Gets the fully qualified element name of the object (i.e. with prefix)
+	 * @return a fully qualified xml element name
+	 */
 	public String getQName () {
 		return this.qName;
 	}
 	
+	/**
+	 * Gets the attributes associated with the object
+	 * @return an Attributes object
+	 */
 	public Attributes getAttributes () {
 		return this.atts;
 	}
 	
+	/**
+	 * Sets the raw character data (e.g. sequence data) for the object
+	 * @param an array of characters
+	 */
 	public void setCharacterData(char[] myCharacters) {
 		this.characters = myCharacters;
 	}
 	
+	/**
+	 * Gets the raw character data (e.g. sequence data) of the object
+	 * @return an array of characters
+	 */
 	public char[] getCharacterData() {
 		return this.characters;
 	}
