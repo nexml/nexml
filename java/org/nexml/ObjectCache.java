eb57c56c5191c8d23ffd5f9d8bd0d0e381e48c59
Rutger Vos <rutgeraldo@gmail.com>
2007-12-04 22:15:55 +0000

    Refactored org.nexml.ObjectCache into an interface

--- java/org/nexml/ObjectCache.java
+++ java/org/nexml/ObjectCache.java
@@ -2,41 +2,27 @@ package org.nexml;
 
 // $Id$
 
-import java.util.*;
-
 /**
- * The ObjectCache stores objects instantiated by the factories and passed
- * to it by the ElementHandler. It indexes them in various ways, so that 
- * references between objects can be resolved as the stream is being 
- * processed. In particular, because the specification is designed such that
- * referenced objects always precede the objects that refer to them, once a
- * reference comes along, the object that it refers to can be retrieved from
- * this cache.
+ * Mediator objects (such as a MesquiteProject) need to implement this interface,
+ * possibly in an adaptor.
  * @author rvosa
  */
-public class ObjectCache {
-	private Hashtable objById;
-	private Hashtable idByObj;
-	private Hashtable referencedIdById;
-	
+public interface ObjectCache {
+
 	/**
 	 * Gets an object from the cache by looking it up by its id
 	 * @param id a nexml id attribute's value
 	 * @return the object associated with the id
 	 */
-	public Object getObjectById (String id) {
-		return this.objById.get(id);
-	}
-	
+	public abstract Object getObjectById(String id);
+
 	/**
 	 * Gets the nexml id attribute's value for the provided object
 	 * @param obj an object for which to look up the id
 	 * @return a string id
 	 */
-	public String getIdByObject(Object obj) {
-		return (String)this.idByObj.get(obj);
-	}
-	
+	public abstract String getIdByObject(Object obj);
+
 	/**
 	 * Gets the id reference associated with the provided id. For
 	 * example, if a characters element has id "c1" and it references
@@ -44,21 +30,14 @@ public class ObjectCache {
 	 * @param  id a nexml id attribute's value
 	 * @return an id reference's value
 	 */
-	public String getReferencedIdById (String id) {
-		return (String)this.referencedIdById.get(id);
-	}
-	
+	public abstract String getReferencedIdById(String id);
+
 	/**
 	 * Adds an object to the cache
 	 * @param obj   an Object to add to the cache
 	 * @param id    the id attribute's value
 	 * @param idref the value of the id reference (if any)
 	 */
-	public void setObject(Object obj, String id, String idref) {
-		this.idByObj.put(obj, id);
-		this.objById.put(id, obj);
-		if ( idref != null ) {
-			this.referencedIdById.put(id, idref);
-		}
-	}
+	public abstract void setObject(Object obj, String id, String idref);
+
 }
