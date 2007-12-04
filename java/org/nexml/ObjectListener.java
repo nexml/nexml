7acd3c645930bc4177d2752436fd2a222c5da87b
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2007-12-04 01:38:53 +0000

    Added JavaDoc to ObjectListener

--- java/org/nexml/ObjectListener.java
+++ java/org/nexml/ObjectListener.java
@@ -1,5 +1,12 @@
 package org.nexml;
 
+// $Id$
+
 public interface ObjectListener {
+	
+	/**
+	 * Executes when a new object becomes available
+	 * @param a newly created object from the stream
+	 */	
 	public void newObjectNotification (Object obj);
 }
