06ddb9e11a2553257cb88d6af07e64a6542159ab
Rutger Vos <rutgeraldo@gmail.com>
2007-12-04 01:16:45 +0000

    Added JavaDoc comments to DefaultObjectListener

--- java/org/nexml/DefaultObjectListener.java
+++ java/org/nexml/DefaultObjectListener.java
@@ -1,6 +1,12 @@
 package org.nexml;
 
+// $Id$
+
 public class DefaultObjectListener implements ObjectListener {
+	/**
+	 * Executes when a new object becomes available
+	 * @param a newly created object from the stream
+	 */
 	public void newObjectNotification(Object obj) {
 		System.out.println("Received new object: " + obj);
 	}
