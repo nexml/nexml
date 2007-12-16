ca2de363ce73a1b690b508cabd270085f14ebffb
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2007-12-16 00:18:20 +0000

    Reorganizing /nexml/perl

--- perl/lib/xs/sequence.pm
+++
@@ -1,8 +0,0 @@
-# $Id$
-package xs::sequence;
-use strict;
-use warnings;
-use xs::recursiveElementPattern;
-our @ISA=qw(xs::recursiveElementPattern);
-sub explain { 'A sequence means that out of the items in the list multiple may appear.' }
-1;
