b776c876dac9f35094df79d2c54851578b5117fc
jeetsukumaran <jeetsukumaran@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2008-01-22 23:22:21 +0000

    modifying data model

--- python/pynexml/characters2.py
+++ python/pynexml/characters2.py
@@ -48,65 +48,77 @@ specializations to handle nucleotide, etc. character types.
 from pynexml import base
 from pynexml import taxa
 
-
-class DiscreteCharacterSymbol(object):
-    
-    def __init__(self, symbol_id, symbol_string, symbol_mappings=None):
-        self.set(symbol_id=symbol_id,
-                 symbol_string=symbol_string,
-                 symbol_mappings=symbol_mappings)
-                 
-    def __str__(self):
-        return str(self.symbol_string)
-        
-    def __repr__(self):
-        return str([self.symbol_id, self.symbol_string, [str(sym) for sym in self.symbol_mappings]])
-                 
-    def set(self, symbol_id, symbol_string, symbol_mappings=None):
-        self.symbol_id = symbol_id
-        self.symbol_string = symbol_string
-        if symbol_mappings is not None:
-            self.symbol_mappings = set(symbol_mappings)
-        else:
-            self.symbol_mappings = set([self])
-
-class DiscreteCharacterSymbols(list):
+class DiscreteCharacterState(base.IdTagged):
     """
-    Discrete character mapper.
+    A character state definition, which can either be a fundamental state or
+    a mapping to a set of other character states (for polymorphic or ambiguous
+    characters).
+    """
+    
+    # multistate enums
+    SINGLE_STATE = 0
+    AMBIGUOUS_STATE = 1
+    POLYMORPHIC_STATE = 2
+    
+    def __init__(self, 
+                 elem_id=None, 
+                 label=None,
+                 symbol=None, 
+                 token=None, 
+                 multistate=DiscreteCharacterState.SINGLE_STATE, 
+                 member_states=None):
+        base.IdTagged.__init__(self, elem_id=elem_id, label=label)
+        self.symbol = symbol
+        self.token = token
+        self.multistate = multistate
+        self.member_states = member_states
+        
+    def fundamental_states(self):
+        """
+        Returns value of self in terms of a set of fundamental states (i.e.,
+        set of single states) that correspond to this state.
+        """
+        if self.member_states is None:
+            return set(self)
+        else:
+            states = set()
+            for state in member_states:
+                states.update(state.fundamental_states())
+            return states
+                 
+    def fundamental_ids(self):
+        """
+        Returns set of id's of all fundamental states to which this state maps.
+        """
+        return set([state.elem_id for state in self.fundamental_states()])
+        
+    def fundamental_symbols(self):
+        """
+        Returns set of symbols of all fundamental states to which this state maps.
+        """
+        return set([state.symbol for state in self.fundamental_states() if state.symbol])  
+        
+    def fundamental_tokens(self):
+        """
+        Returns set of tokens of all fundamental states to which this state maps.
+        """
+        return set([state.token for state in self.fundamental_states() if state.token])        
+        
+class DiscreteCharacterStateList(list):
+    """
+    A list of states available for a particular character type/format.
     """
     
     def __init__(self, *args, **kwargs):
-        list.__init__(self, *args)
-        
-    def symbols_by_id(self, ids):
-        return [sym for sym in self if sym.symbol_id in ids]
-        
-    def symbols_by_string(self, str):
-        return [sym for sym in self if sym.symbol_string in str]
-        
-    def symbol_ids(self):
-        return [sym.symbol_id for sym in self]
-        
-    def symbol_strings(self):
-        return [sym.symbol_string for sym in self]
-        
-    def symbol_mappings(self):
-        return [sym.symbol_mappings for sym in self]        
-        
-    def string_to_symbol(self, token):
-        if len(token) == 1:
-            ### TODO: catch index exception and rethrow with
-            ### meaningful message
-            idx = self.symbol_strings().index(token)
-            return self[idx]
-        else:
-            symbol_set = set()
-            for subtoken in token:
-                symbol_set.add(self.string_to_symbol(subtoken))
-            for symbol in self:
-                if symbol_set == symbol.symbol_mappings:
-                    return symbol
-            raise IndexError("No symbol corresponding to '%s' found" % token)
+        list.__init__(self, *args)    
+    
+    def state_by_symbol(self, symbol):
+        """
+        Returns a DiscreteCharacterState object corresponding to the given symbol.
+        """
+        for state in self:
+            if state.symbol == symbol:
+                return state
                 
     
 class DnaCharacterSymbols(DiscreteCharacterSymbols):
