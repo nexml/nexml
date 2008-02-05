d09d1a2d374453b028dd4cdc64adf9c5733775c7
Jeet Sukumaran <jeet@ku.edu>
2008-02-05 23:24:15 +0000

    hooked-in python test harness to top-level regression test script

--- regression_test.sh
+++ regression_test.sh
@@ -7,4 +7,6 @@ echo "  examples/mesquite.xml"
 echo "They will be skipped. Other example files should validate..."
 set -x
 java/validator/validate_nexml.sh examples/TreeInfer.xml examples/characters.xml examples/nexml.xml examples/rest.xml examples/taxa.xml examples/trees.xml examples/verbose.xml 
+
+# python test suite
 python python/setup.py test
