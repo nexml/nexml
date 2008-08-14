7588d6f14e5f71dc355aaa2b882a4532dd2abfa0
Mark Holder <mtholder@ku.edu>
2008-08-14 21:51:08 +0000

    cpp-example/to_nexus/ml2us building, but not working

--- nexml/cpp-example/to_nexus/ml2us.cpp
+++ nexml/cpp-example/to_nexus/ml2us.cpp
@@ -15,23 +15,70 @@ class NexmlCharactersAdaptor
 class NexmlCharactersAdaptorSeqs : public NexmlCharactersAdaptor
 {
 	public:
-		NexmlCharactersAdaptorSeqs(const AbstractSeqs *p):raw(p){}
+		NexmlCharactersAdaptorSeqs(const AbstractSeqs * p):raw(p){}
 		virtual unsigned getNumChars() const {
 			if (!(this->raw))
 				return 0;
-			const AbstractSeqMatrix & mat = raw->matrix();
-			const AbstractSeqMatrix::row_sequence & s = mat.row();
-			if (s.empty())
-				return 0;
-			const AbstractSeqRow & fr = *(s.begin());
-			const DNAMatrixSeqRow * dsr = dynamic_cast<const DNAMatrixSeqRow *> (&fr);
-			if (dsr) {
-				const AbstractSeq & fr_seq = dsr->seq();
-				
-			}
-				
-			const AbstractSeqRow::seq_type & fr_seq = fr.seq();
-			cout << "fr_seq is " << fr_seq << '\n';
+			cout << "typeid(raw).name() = " << typeid(raw).name() << "\n";
+			const DnaSeqs * ds = dynamic_cast<const DnaSeqs *>(raw);
+			cout << "typeid(ds).name() = " << typeid(ds).name() << "\n";
+			if (ds)
+				{
+				cout << "DnaSeqs\n";
+				const AbstractSeqMatrix & mat = ds->matrix();
+				const AbstractSeqMatrix * matP = &mat;
+				const DNASeqMatrix * dmat = dynamic_cast<const DNASeqMatrix *> (matP);
+				if (dmat) {
+					cout << "DNASeqMatrix obj: " << *dmat << '\n';
+				}
+				else {
+					cout << "not DNASeqMatrix\n";
+					cout << "typeid(mat).name() = " << typeid(mat).name() << "\n";
+					if (dynamic_cast<const RNASeqMatrix *> (matP))
+						cout << "RNASeqMatrix\n";
+					else if (dynamic_cast<const AASeqMatrix *> (matP))
+						cout << "AASeqMatrix\n";
+					else if (dynamic_cast<const ContinuousSeqMatrix *> (matP))
+						cout << "ContinuousSeqMatrix\n";
+					else if (dynamic_cast<const RestrictionSeqMatrix *> (matP))
+						cout << "RestrictionSeqMatrix\n";
+					else if (dynamic_cast<const StandardSeqMatrix *> (matP))
+						cout << "StandardSeqMatrix\n";
+					else
+						cout << "dynamic cast not succeeding for any known SeqMatrix\n";
+					exit(1);
+				}
+				}
+			else
+				{
+				cout << "not DnaSeqs\n";
+				const AbstractSeqMatrix & mat = raw->matrix();
+					
+				const AbstractSeqMatrix::row_sequence & s = mat.row();
+				if (s.empty())
+					return 0;
+				const AbstractSeqRow & fr = *(s.begin());
+				cout << s.size() << " rows\n";
+				const DNAMatrixSeqRow * dsr = dynamic_cast<const DNAMatrixSeqRow *> (&fr);
+				if (dsr) {
+					cout << "DNAMatrixSeqRow obj: " << *dsr << '\n';
+					const xsd::cxx::tree::simple_type<xml_schema::type> fr_seq = dsr->seq();	
+					const xsd::cxx::tree::simple_type<xml_schema::type> *fp = &fr_seq;
+					const DNASeq * d = dynamic_cast<const DNASeq *>(fp);
+					if (d) {
+						cout << "DNASeq obj: " << *d << '\n';
+					}		
+				}
+				else
+					cout << "not DNAMatrixSeqRow\n";
+					
+				const AbstractSeqRow::seq_type & fr_seq = fr.seq();
+				const DNASeq * d = dynamic_cast<const DNASeq *>((&fr_seq));
+				if (d) 
+					cout << "DNASeq obj: " << *d << '\n';
+				else
+					cout << "fr_seq is " << fr_seq << '\n';
+				}
 			return 1;
 		}
 	protected:
@@ -50,7 +97,7 @@ class NexmlCharactersAdaptorCells : public NexmlCharactersAdaptor
 const NexmlCharactersAdaptor * createCharactersAdaptor( const AbstractBlock *);
 
 
-const NexmlCharactersAdaptor * createCharactersAdaptor( const AbstractBlock * ab)
+const NexmlCharactersAdaptor * createCharactersAdaptor(const AbstractBlock * ab)
 {
 	if (!ab)
 		return NULL;
@@ -87,28 +134,36 @@ int main (int argc, char* argv[])
 				cout <<  "unadaptable characters\n";
 			// now try to cast to one of the concrete subclasses
 			if (RestrictionSeqs* rs = dynamic_cast<RestrictionSeqs*> (&ab)) {
+			cout << "restrict type:\n";
 			cout << *rs << endl;
 			}
 			else if (ContinuousSeqs* cs = dynamic_cast<ContinuousSeqs*> (&ab)) {
+			cout << "contin seq type:\n";
 			cout << *cs << endl;
 			}
 			else if (StandardSeqs* ss = dynamic_cast<StandardSeqs*> (&ab)) {
+			cout << "std cells type:\n";
 			cout << *ss << endl;
 			}
 			else if (StandardCells* sc = dynamic_cast<StandardCells*> (&ab)) {
+			cout << "std type:\n";
 			cout << *sc << endl;
 			}
 			else if (ContinuousCells* cc = dynamic_cast<ContinuousCells*> (&ab)) {
+			cout << "contin. cell type:\n";
 			cout << *cc << endl;
 			}
 			else if (DnaSeqs* ds = dynamic_cast<DnaSeqs*> (&ab)) {
+			cout << "dna type:\n";
 			cout << *ds << endl;
 			}
 			else if (RnaSeqs* rs = dynamic_cast<RnaSeqs*> (&ab)) {
+			cout << "rna type:\n";
 			cout << *rs << endl;
 			}
 			else {
 				// Print the base type.
+				cout << "base type:\n";
 				cout << ab << endl;
 			}
 		}
