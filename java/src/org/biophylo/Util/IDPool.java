e7b6f925311ce679c4bdd57d6f1874f70b6a66ee
Rutger Vos <rutgeraldo@gmail.com>
2008-10-24 04:03:52 +0000

    Added javadoc to ./Util/IDPool.java

--- java/src/org/biophylo/Util/IDPool.java
+++ java/src/org/biophylo/Util/IDPool.java
@@ -3,15 +3,27 @@ package org.biophylo.Util;
 public class IDPool {
 	private static IDPool instance = null;
 	private int counter;
+	
+	/**
+	 * 
+	 */
 	protected IDPool() {
 	     this.counter = 0;
 	}
+	
+	/**
+	 * @return
+	 */
 	public static IDPool getInstance() {
 		if(instance == null) {
 			instance = new IDPool();
 		}
 	    return instance;
 	}
+	
+	/**
+	 * @return
+	 */
 	public int makeId () {
 		return this.counter++;
 	}
