5602e28ee0a5b1ccf9107e4c0d967cbc10703418
Rutger Vos <rutgeraldo@gmail.com>
2008-10-24 03:57:45 +0000

    Added javadoc to ./Containable.java

--- nexml/java/src/org/biophylo/Containable.java
+++ nexml/java/src/org/biophylo/Containable.java
@@ -3,9 +3,17 @@ import org.biophylo.Util.*;
 public abstract class Containable extends XMLWritable {	
 	protected int container;
 	protected int type;
+	
+	/**
+	 * @return
+	 */
 	public int container () {
 		return this.container;
 	}
+	
+	/**
+	 * @return
+	 */
 	public int type () {
 		return this.type;
 	}
