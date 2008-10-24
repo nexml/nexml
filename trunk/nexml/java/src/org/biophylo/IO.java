59179a1078a1cebff6053161ee1b3c9dcd82e802
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2008-10-24 03:58:36 +0000

    Added javadoc to ./IO.java

--- trunk/nexml/java/src/org/biophylo/IO.java
+++ trunk/nexml/java/src/org/biophylo/IO.java
@@ -3,6 +3,11 @@ import java.io.*;
 import org.biophylo.Parsers.*;
 import org.biophylo.Matrices.Datatype.Datatype;
 public class IO {
+	/**
+	 * @param format
+	 * @param data
+	 * @return
+	 */
 	public static Object[] parse(String format, InputStream data) {
 		String base = "org.biophylo.Parsers.";
 		Parsers p = null;
