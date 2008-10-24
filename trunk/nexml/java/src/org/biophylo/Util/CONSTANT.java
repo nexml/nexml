7f654799255349e25d0f95ef5410af099d1bde87
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2008-10-24 02:31:16 +0000

    Adding project class and xml Element writing

--- trunk/nexml/java/src/org/biophylo/Util/CONSTANT.java
+++ trunk/nexml/java/src/org/biophylo/Util/CONSTANT.java
@@ -3,15 +3,16 @@ import org.biophylo.*;
 import org.biophylo.Util.Exceptions.*;
 
 public class CONSTANT {
-	public static final int NONE = 1;
-	public static final int NODE = 2;
-	public static final int TREE = 3;
-	public static final int FOREST = 4;
-	public static final int TAXON = 5;
-	public static final int TAXA = 6;
-	public static final int DATUM = 7;
-	public static final int MATRIX = 8;
+	public static final int NONE     = 1;
+	public static final int NODE     = 2;
+	public static final int TREE     = 3;
+	public static final int FOREST   = 4;
+	public static final int TAXON    = 5;
+	public static final int TAXA     = 6;
+	public static final int DATUM    = 7;
+	public static final int MATRIX   = 8;
 	public static final int MATRICES = 9;
+	public static final int PROJECT  = 10;
 	public static final boolean looksLikeObject(Containable obj, int constant) throws Generic {
 		if ( obj.type() == constant ) {
 			return true;
