c995573bb43b6289a3e6ada84137804019095310
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2008-10-24 03:59:48 +0000

    Added javadoc to ./Matrices/Datatype/Standard.java

--- trunk/nexml/java/src/org/biophylo/Matrices/Datatype/Standard.java
+++ trunk/nexml/java/src/org/biophylo/Matrices/Datatype/Standard.java
@@ -14,14 +14,23 @@ public class Standard extends Datatype {
 		{0,0,0,0,0,0,0,0,0,1},
 	};	
 	
+	/* (non-Javadoc)
+	 * @see org.biophylo.Matrices.Datatype.Datatype#isValueConstrained()
+	 */
 	public boolean isValueConstrained() {
 		return false;
 	}	
 	
+	/* (non-Javadoc)
+	 * @see org.biophylo.Matrices.Datatype.Datatype#isSequential()
+	 */
 	public boolean isSequential() {
 		return false;
 	}	
 	
+	/**
+	 * 
+	 */
 	public Standard() {
 		this.alphabet = "0123456789";
 		this.missing = '?';
