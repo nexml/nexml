d6b475e2913bbfeab38e1193dbd242b928b4ee2e
Rutger Vos <rutgeraldo@gmail.com>
2008-10-24 04:01:49 +0000

    Added javadoc to ./Taxa/TaxaLinker.java

--- java/src/org/biophylo/Taxa/TaxaLinker.java
+++ java/src/org/biophylo/Taxa/TaxaLinker.java
@@ -1,7 +1,18 @@
 package org.biophylo.Taxa;
 
 public interface TaxaLinker {
+	/**
+	 * @return
+	 */
 	public Taxa getTaxa();
+	
+	/**
+	 * @param taxa
+	 */
 	public void setTaxa(Taxa taxa);
+	
+	/**
+	 * 
+	 */
 	public void unsetTaxa();
 }
