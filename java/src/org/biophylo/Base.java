77201e4d5895af15cae7db1fa4821b2559a81cc7
Rutger Vos <rutgeraldo@gmail.com>
2008-10-24 03:57:31 +0000

    Added javadoc to ./Base.java

--- java/src/org/biophylo/Base.java
+++ java/src/org/biophylo/Base.java
@@ -12,37 +12,40 @@ import java.lang.reflect.*;
  */
 public abstract class Base {
 	private String name;
-	private String desc;
-	private Double score;
-	private HashMap generic;
+	private Map generic;
 	private int id;	
 	private static Logger logger = Logger.getInstance();
 	private IDPool pool = IDPool.getInstance();
 	private static ObjectMediator objectMediator = ObjectMediator.getInstance();
 	public static final double VERSION = 0.1;
 	
+	/**
+	 * 
+	 */
 	public Base () {
 		this.id = pool.makeId();
 		this.generic = new HashMap();
 		objectMediator.register(this);
 	}
 	
+	/**
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-	
-	public void setScore(Double score) {
-		this.score = score;
-	}
-	
-	public void setGeneric(HashMap generic) {
+	/**
+	 * @param generic
+	 */
+	public void setGeneric(Map generic) {
 		this.generic = generic;
 	}
 	
+	/**
+	 * @param key
+	 * @param value
+	 */
 	public void setGeneric(Object key, Object value) {
 		if ( this.generic == null ) {
 			this.generic = new HashMap();
@@ -50,38 +53,52 @@ public abstract class Base {
 		this.generic.put(key, value);
 	}
 	
+	/**
+	 * @return
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * @return
+	 */
 	public String getInternalName() {
 		return this.getClass().getSimpleName() + this.getId();
 	}
 	
-	public String getDesc() {
-		return this.desc;
-	}
-	
-	public Double getScore() {
-		return this.score;
-	}
-	
-	public HashMap getGeneric () {
+	/**
+	 * @return
+	 */
+	public Map getGeneric () {
 		return this.generic;
 	}
 	
+	/**
+	 * @param key
+	 * @return
+	 */
 	public Object getGeneric (Object key) {
 		return this.generic.get(key);
 	}
 	
+	/**
+	 * @return
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * @return
+	 */
 	public static Logger getLogger() {
 		return logger;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#finalize()
+	 */
 	protected void finalize() throws Throwable {
 	  //do finalization here
 		objectMediator.unregister(this);
