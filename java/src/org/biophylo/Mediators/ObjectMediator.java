153fcd5a27c9b5bf29d155d713957f599c6aaa0d
Rutger Vos <rutgeraldo@gmail.com>
2008-10-24 04:00:33 +0000

    Added javadoc to ./Mediators/ObjectMediator.java

--- java/src/org/biophylo/Mediators/ObjectMediator.java
+++ java/src/org/biophylo/Mediators/ObjectMediator.java
@@ -5,22 +5,43 @@ import java.util.*;
 public class ObjectMediator {
 	private static ObjectMediator instance = null;
 	private Vector object;
+	
+	/**
+	 * 
+	 */
 	protected ObjectMediator() {
 	     this.object = new Vector();
 	}
+	
+	/**
+	 * @return
+	 */
 	public static ObjectMediator getInstance() {
 		if(instance == null) {
 			instance = new ObjectMediator();
 		}
 	    return instance;
 	}
+	
+	/**
+	 * @param obj
+	 */
 	public void register(Base obj) {
 		int id = obj.getId();
 		this.object.add(id, obj);
 	}
+	
+	/**
+	 * @param id
+	 * @return
+	 */
 	public Base getObjectById(int id) {
 		return (Base)this.object.get(id);
 	}
+	
+	/**
+	 * @param obj
+	 */
 	public void unregister(Base obj) {
 		this.object.remove(obj);
 	}
