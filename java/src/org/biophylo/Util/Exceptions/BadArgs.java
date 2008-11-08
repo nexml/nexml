de7b88f4b23cdba0ce5e300b5ef0c53d713426fa
Rutger Vos <rutgeraldo@gmail.com>
2008-11-08 00:25:01 +0000

    NAMESPACE RENAMING

--- java/src/org/biophylo/Util/Exceptions/BadArgs.java
+++
@@ -1,5 +0,0 @@
-package org.biophylo.Util.Exceptions;
-
-public class BadArgs extends Generic {
-	public String description = "This kind of error happens when bad or incomplete arguments are provided.";
-}
--- java/src/org/biophylo/Util/Exceptions/ObjectMismatch.java
+++
@@ -1,5 +0,0 @@
-package org.biophylo.Util.Exceptions;
-
-public class ObjectMismatch extends BadArgs {
-	public String description = "This kind of error happens when an invalid object argument is provided.";
-}
