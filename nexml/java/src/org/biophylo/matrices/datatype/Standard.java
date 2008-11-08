ae335538ccb0bc3fa56c39e487f68f13ca421a7e
Rutger Vos <rutgeraldo@gmail.com>
2008-11-08 11:30:29 +0000

    Fixed bug in "standard" data validation (which are space-separated integers)

--- nexml/java/src/org/biophylo/matrices/datatype/Standard.java
+++ nexml/java/src/org/biophylo/matrices/datatype/Standard.java
@@ -36,4 +36,41 @@ public class Standard extends Datatype {
 		this.missing = '?';
 		this.lookup = standardlookup;
 	}
+	
+	/* (non-Javadoc)
+	 * @see org.biophylo.Matrices.Datatype.Datatype#join(java.lang.String[])
+	 */
+	public String join(String[] chars) {
+		StringBuffer sb = new StringBuffer();
+		for ( int i = 0; i < chars.length; i++ ) {
+			if ( i > 0 ) {
+				sb.append(' ');
+			}
+			sb.append(chars[i]);
+		}
+		return sb.toString();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.biophylo.Matrices.Datatype.Datatype#split(java.lang.String)
+	 */
+	public String[] split(String chars) {
+		return chars.split(" ");
+	}	
+
+	public boolean isValid(String charsString) {
+		String[] chars = split(charsString.toUpperCase());
+		String missing = "" + getMissing();
+		String gap = "" + getGap();
+		for ( int i = 0; i < chars.length; i++ ) {
+			if ( chars[i].length() == 0 ) {
+				continue;
+			}
+			if ( !gap.equals(chars[i]) && !missing.equals(chars[i]) && ! chars[i].matches("[0-9]+") ) {
+				return false;
+			}
+		}
+		return true;
+	}	
+
 }
