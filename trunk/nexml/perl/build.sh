cd5c42a957ebdfd28c317661bd5dd53a7de4671f
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2009-02-22 21:16:43 +0000

    Redirecting boiler plate noise to /dev/null

--- trunk/nexml/perl/build.sh
+++ trunk/nexml/perl/build.sh
@@ -2,9 +2,9 @@
 # other than on the nexml webserver. It builds a CPAN compatible
 # distribution out of the source tree, places that in the downloads
 # folder, then cleans up after itself. That's all.
-perl Makefile.PL
-make manifest
-make dist
-make clean
+perl Makefile.PL > /dev/null
+make manifest &> /dev/null
+make dist > /dev/null
+make clean > /dev/null
 mv Bio-Phylo*.tar.gz ../downloads
 rm Makefile.old MANIFEST.bak MANIFEST META.yml
