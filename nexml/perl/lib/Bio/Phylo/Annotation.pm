1c9c4150593724e8e31be30f73c2731562bb21d1
Rutger Vos <rutgeraldo@gmail.com>
2009-02-22 05:16:04 +0000

    Removed redundant methods, added support for DOM attachments to "any" dictionary values.

--- nexml/perl/lib/Bio/Phylo/Annotation.pm
+++ nexml/perl/lib/Bio/Phylo/Annotation.pm
@@ -3,7 +3,8 @@ use strict;
 use Bio::Phylo::Util::XMLWritable;
 use Bio::Phylo::Util::CONSTANT qw(_ANNOTATION_ _DICTIONARY_);
 use vars '@ISA';
-use UNIVERSAL 'can';
+use UNIVERSAL qw'can isa';
+use Bio::Phylo::Util::Exceptions 'throw';
 @ISA=qw(Bio::Phylo::Util::XMLWritable);
 {
     my @fields = \( my ( %key, %datatype, %value ) );
@@ -11,34 +12,12 @@ use UNIVERSAL 'can';
     my $CONTAINER_CONSTANT = _DICTIONARY_;
     
     sub new {
-        return shift->SUPER::new( '-tag' => 'key', @_ );     
-    }
-    
-    sub set_key {
-        my $self = shift;
-        $key{ $self->get_id } = shift;
-        return $self;
-    }
-    
-    sub set_type {
-        my $self = shift;
-        $datatype{ $self->get_id } = shift;
-        return $self;
+        return shift->SUPER::new( '-tag' => 'string', @_ );     
     }
     
     sub set_value {
-        my $self = shift;
-        $value{ $self->get_id } = shift;
-    }
-    
-    sub get_key {
-        my $self = shift;
-        return $key{ $self->get_id };
-    }
-    
-    sub get_type {
-        my $self = shift;
-        return $datatype{ $self->get_id };
+        my ( $self, $value ) = @_;
+        $value{ $self->get_id } = $value;
     }
     
     sub get_value {
@@ -48,16 +27,62 @@ use UNIVERSAL 'can';
     
     sub to_xml {
         my $self = shift;
-        my $key = $self->get_key || $self->get_xml_id;
-        my $xml = '<key>' . $key . '</key>';
+        my $key = $self->get_xml_id;
+        my $xml = '';
         my $value = $self->get_value;
-        if ( not can( $value, 'to_xml' ) ) {
-            my $type = $self->get_type || 'string';        
-            $xml .= "<${type}>" . $value . "</${type}>";
+        if ( ref($value) ) {
+
+        	# for RDF::Core::Model objects
+        	if ( isa($value, 'RDF::Core::Model') ) {
+        		eval {
+        			require RDF::Core::Model::Serializer;
+        			my $serialized_model = '';
+  					my $serializer = RDF::Core::Model::Serializer->new(
+  						'Model'  => $value,
+                        'Output' => \$serialized_model,
+                        # BaseURI => 'URI://BASE/',
+                    );   
+                    $value = $serialized_model;     			
+        		};
+        		if ( $@ ) {
+        			throw 'API' => $@;
+        		}
+        	}         
+        	
+        	# for XML::XMLWriter object
+        	elsif ( isa($value, 'XML::XMLWriter') ) {
+        		$value = $value->get;
+        	}
+        	
+        	else {
+        		my $concatenated = '';
+        		my @values;
+        		if ( ref($value) eq 'ARRAY' ) {
+        			@values = @{ $value };
+        		}
+        		else {
+        			push @values, $value;
+        		}
+        		for my $v ( @values ) {
+	        		# duck-typing
+		        	# Bio::Phylo => to_xml, XML::DOM,XML::GDOME => toString, XML::Twig => sprint
+		        	# XML::DOM2 => xmlify, XML::DOMBacked => as_xml,
+		        	# XML::Handler => dump_tree, XML::Element => as_XML
+		        	# XML::API => _as_string, XML::Code => code
+	
+		        	my @methods = qw(to_xml toString sprint _as_string code xmlify as_xml dump_tree as_XML);
+		        	SERIALIZER: for my $method ( @methods ) {
+		        		if ( can($v,$method) ) {
+		        			$concatenated .= $v->$method;
+		        			last SERIALIZER;
+		        		}
+		        	}
+        		}
+        		$value = $concatenated;
+        	}
         }
-        else {
-            $xml .= $value->to_xml;
-        }
+        my $type = $self->get_tag;
+        $xml .= "<${type} id=\"${key}\">" . $value . "</${type}>";
         return $xml;
     }
     
