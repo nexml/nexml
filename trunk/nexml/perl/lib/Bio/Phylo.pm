761e66b835208a71a0847b9c72b051ec2b1292fc
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2009-03-03 01:25:54 +0000

    Changed release candidate number to 8

--- trunk/nexml/perl/lib/Bio/Phylo.pm
+++ trunk/nexml/perl/lib/Bio/Phylo.pm
@@ -37,7 +37,7 @@ require Bio::Phylo::Mediators::TaxaMediator;
 # Include the revision number from CIPRES subversion in $VERSION
 my $rev = '$Id$';
 $rev =~ s/^[^\d]+(\d+)\b.*$/$1/;
-$VERSION = "0.17_RC7";
+$VERSION = "0.17_RC8";
 $VERSION .= "_$rev";
 {
     my $taxamediator = 'Bio::Phylo::Mediators::TaxaMediator';
