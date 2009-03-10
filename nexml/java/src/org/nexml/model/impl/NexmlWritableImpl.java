707670cf9a0d98f920a8fa88fc564c80ffe1e2b6
Sam Donnelly <sdonnelly1@users.sourceforge.net>
2009-03-10 16:35:28 +0000

    Nexml interfaces.

---
+++ nexml/java/src/org/nexml/model/impl/NexmlWritableImpl.java
@@ -0,0 +1,20 @@
+package org.nexml.model.impl;
+
+import org.nexml.model.NexmlWritable;
+
+public abstract class NexmlWritableImpl implements NexmlWritable {
+
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setLabel(String label) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	abstract String getTagName();
+
+		
+}
