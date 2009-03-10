c8fdfaa3e7fcf7244d488d0949b09db83e1d1438
Sam Donnelly <sdonnelly1@users.sourceforge.net>
2009-03-10 19:42:01 +0000

    Nexml interfaces + testing + impl.

---
+++ nexml/java/src/org/nexml/model/impl/OTUImpl.java
@@ -0,0 +1,33 @@
+package org.nexml.model.impl;
+
+import org.nexml.model.Dictionary;
+import org.nexml.model.OTU;
+
+public class OTUImpl extends NexmlWritableImpl implements OTU {
+
+	public void addDictionary(Dictionary dictionary) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Dictionary getDictionary() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLabel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setLabel(String label) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	String getTagName() {
+		return "otu";
+	}
+
+}
