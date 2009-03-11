9f83b78d88f11fdc6e903a39aaa619519ffe9c2c
Sam Donnelly <sdonnelly1@users.sourceforge.net>
2009-03-11 21:15:36 +0000

    Nexml interfaces + testing + impl.

--- nexml/java/src/org/nexml/model/impl/NetworkImpl.java
+++ nexml/java/src/org/nexml/model/impl/NetworkImpl.java
@@ -1,6 +1,8 @@
 package org.nexml.model.impl;
 
-import org.nexml.model.Dictionary;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.nexml.model.Edge;
 import org.nexml.model.Network;
 import org.nexml.model.NetworkObject;
@@ -8,47 +10,13 @@ import org.nexml.model.Node;
 
 public abstract class NetworkImpl<E extends Edge> extends SetManager<NetworkObject> implements Network<E> {
 
-	public void addEdge(E edge) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public Dictionary createDictionary() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Dictionary getDictionary() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getLabel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setDictionary(Dictionary dictionary) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void setLabel(String label) {
-		// TODO Auto-generated method stub
-	}
 
 	@Override
 	String getTagName() {
 		return "network";
 	}
 
-	public void addNode(Node node) {
-		// TODO Auto-generated method stub
-
-	}
-
 	abstract public E createEdge();
-
 
 	public Node createNode() {
 		Node node = new NodeImpl();
@@ -56,24 +24,34 @@ public abstract class NetworkImpl<E extends Edge> extends SetManager<NetworkObje
 		return node;
 	}
 
-	public E getEdge(String edgeId) {
-		// TODO Auto-generated method stub
-		return null;
+	@SuppressWarnings("unchecked")
+	public Set<E> getEdges() { 
+		Set<E> edges = new HashSet<E>();
+		for (NetworkObject networkObject : getThings()) { 
+			if (networkObject instanceof Edge) { 
+				edges.add((E)networkObject);
+			}
+		}
+		return edges;
 	}
-
-	public Node getNode(String nodeId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	public void removeEdge(E edge) {
-		// TODO Auto-generated method stub
-
+		removeThing(edge);
 	}
 
+	public Set<Node> getNodes() { 
+		Set<Node> nodes = new HashSet<Node>();
+		for (NetworkObject networkObject : getThings()) { 
+			if (networkObject instanceof Node) { 
+				nodes.add((Node)networkObject);
+			}
+		}
+		return nodes;
+	}
+	
+	
 	public void removeNode(Node node) {
-		// TODO Auto-generated method stub
-
+		removeThing(node);
 	}
 
 }
