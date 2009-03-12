2737f3ac60487a22e27a580897d596d4ca3a6413
Rutger Vos <rutgeraldo@gmail.com>
2009-03-12 14:29:17 +0000

    Passing DOM document up constructor

--- java/src/org/nexml/model/impl/CharacterImpl.java
+++ java/src/org/nexml/model/impl/CharacterImpl.java
@@ -2,8 +2,14 @@ package org.nexml.model.impl;
 
 import org.nexml.model.Character;
 import org.nexml.model.CharacterStateSet;
+import org.w3c.dom.Document;
 
 class CharacterImpl extends NexmlWritableImpl implements Character {
+
+	public CharacterImpl(Document document) {
+		super(document);
+		// TODO Auto-generated constructor stub
+	}
 
 	private CharacterStateSet mCharacterStateSet;
 
