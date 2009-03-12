02f7b80a99daf8fb68da4ad7556b2442ea16b8bb
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2009-03-12 14:18:38 +0000

    Creating DOM document obj here, pass it up chained constructors.

--- trunk/nexml/java/src/org/nexml/model/DocumentFactory.java
+++ trunk/nexml/java/src/org/nexml/model/DocumentFactory.java
@@ -1,20 +1,46 @@
 package org.nexml.model;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.nexml.model.impl.DocumentImpl;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class DocumentFactory {
-	static public Document createDocument() {
-		return new DocumentImpl();
+	static public Document createDocument() throws ParserConfigurationException {		
+		return new DocumentImpl(getDocumentBuilder().newDocument());
 	}
 	
-	static public Document parse(InputStream is) {
-		return null;
+	static public Document parse(InputStream inputStream) throws ParserConfigurationException, SAXException, IOException  {
+		Document document = new DocumentImpl(getDocumentBuilder().parse(inputStream));
+		return document;
 	}
 	
-	static public Document parse(String uri) { 
-		return null;
+	static public Document parse(String uri) throws SAXException, IOException, ParserConfigurationException { 
+		Document document = new DocumentImpl(getDocumentBuilder().parse(uri));
+		return document;
+	}
+	
+	static public Document parse(File file) throws SAXException, IOException, ParserConfigurationException {
+		Document document = new DocumentImpl(getDocumentBuilder().parse(file));
+		return document;
+	}
+	
+	static public Document parse(InputSource inputSource) throws SAXException, IOException, ParserConfigurationException {
+		Document document = new DocumentImpl(getDocumentBuilder().parse(inputSource));
+		return document;
+	}
+	
+	static private DocumentBuilder getDocumentBuilder () throws ParserConfigurationException {
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		return documentBuilder;
 	}
 	
 	//etc.
