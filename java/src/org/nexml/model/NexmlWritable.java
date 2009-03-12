b9eb16709b47374f444ac40d508b09e407b080c5
Rutger Vos <rutgeraldo@gmail.com>
2009-03-12 14:15:44 +0000

    Added getId() to interface

--- java/src/org/nexml/model/NexmlWritable.java
+++ java/src/org/nexml/model/NexmlWritable.java
@@ -5,6 +5,8 @@ package org.nexml.model;
  * All {@code NexmlWritable}s are annotatable.
  */
 public interface NexmlWritable {
+	String DEFAULT_NAMESPACE = "http://www.nexml.org/1.0";
+
 	String getLabel();
 
 	void setLabel(String label);
@@ -14,5 +16,7 @@ public interface NexmlWritable {
 	void setDictionary(Dictionary dictionary);
 
 	Dictionary createDictionary();
+	
+	String getId();
 	
 }
