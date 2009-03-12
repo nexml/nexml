fcbd902817d01668d5ffe54cd8ab8fb3bfb0423d
Jim Balhoff <balhoff@nescent.org>
2009-03-12 20:12:25 +0000

    Inserted AnnotatableImpl under NexmlWritableImpl.

--- nexml/java/src/org/nexml/model/impl/AnnotatableImpl.java
+++ nexml/java/src/org/nexml/model/impl/AnnotatableImpl.java
@@ -1,68 +1,29 @@
 package org.nexml.model.impl;
 
 import java.util.Set;
-import java.util.UUID;
 
 import org.nexml.model.Annotatable;
-import org.nexml.model.Annotation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-abstract class AnnotatableImpl implements Annotatable {
-	private Document mDocument = null;
-	private String mId;
-	private Set<Annotation> mAnnotations;
-	private Element mElement;
-	protected static String XSI_NS = "http://www.w3.org/2001/XMLSchema-instance";
-	protected static String XSI_PREFIX = "xsi";
-	protected static String NEX_PREFIX = "nex";
+abstract class AnnotatableImpl extends NexmlWritableImpl implements Annotatable {
+    
+    public AnnotatableImpl(Document document, Element element) {
+        super(document, element);
+    }
 
-	protected AnnotatableImpl() {
-	}
-	
-	public AnnotatableImpl(Document document) {
-		mId = "a" + UUID.randomUUID();
-		mDocument = document;
-		mElement = document.createElementNS(DEFAULT_NAMESPACE, getTagName());
-		getElement().setAttribute("id", getId());
-	}	
-	
-	public AnnotatableImpl(Document document,Element element) {
-		mId = "a" + UUID.randomUUID();
-		mDocument = document;
-		mElement = element;		
-		getElement().setAttribute("id", getId());
-	}	
-	
-	protected final Document getDocument() {
-		return mDocument;
-	}
-	
-	public final Element getElement() {
-		return mElement;
-	}
-	
-	public String getLabel() {
-		return getElement().getAttribute("label");
-	}
+    public AnnotatableImpl(Document document) {
+        super(document);
+    }
+    
+    protected AnnotatableImpl() {}
 
-	public void setLabel(String label) {
-		getElement().setAttribute("label", label);
-	}
-	
-	public String getId() { 
-		return mId;
-	}
-	
-	public Set<Object> getAnnotationValues(String property) {
-	 //TODO
-	    return null;
-	}
-	    
-	public void setAnnotationValue(String property, Object value) {
-	    //TODO
-	}
-	
-	abstract String getTagName();
-		
+    public Set<Object> getAnnotationValues(String property) {
+        //TODO
+           return null;
+       }
+           
+       public void setAnnotationValue(String property, Object value) {
+           //TODO
+       }
 }
---
+++ nexml/java/src/org/nexml/model/impl/NexmlWritableImpl.java
@@ -0,0 +1,59 @@
+package org.nexml.model.impl;
+import java.util.Set;
+import java.util.UUID;
+
+import org.nexml.model.Annotation;
+import org.nexml.model.NexmlWritable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+
+public abstract class NexmlWritableImpl implements NexmlWritable {
+    private Document mDocument = null;
+    private String mId;
+    private Set<Annotation> mAnnotations;
+    private Element mElement;
+    protected static String XSI_NS = "http://www.w3.org/2001/XMLSchema-instance";
+    protected static String XSI_PREFIX = "xsi";
+    protected static String NEX_PREFIX = "nex";
+
+    protected NexmlWritableImpl() {
+    }
+    
+    public NexmlWritableImpl(Document document) {
+        mId = "a" + UUID.randomUUID();
+        mDocument = document;
+        mElement = document.createElementNS(DEFAULT_NAMESPACE, getTagName());
+        getElement().setAttribute("id", getId());
+    }   
+    
+    public NexmlWritableImpl(Document document,Element element) {
+        mId = "a" + UUID.randomUUID();
+        mDocument = document;
+        mElement = element;     
+        getElement().setAttribute("id", getId());
+    }   
+    
+    protected final Document getDocument() {
+        return mDocument;
+    }
+    
+    public final Element getElement() {
+        return mElement;
+    }
+    
+    public String getLabel() {
+        return getElement().getAttribute("label");
+    }
+
+    public void setLabel(String label) {
+        getElement().setAttribute("label", label);
+    }
+    
+    public String getId() { 
+        return mId;
+    }
+    
+    abstract String getTagName();
+        
+}
