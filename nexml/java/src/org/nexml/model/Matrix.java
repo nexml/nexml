56ad401ef898fe0d37b10a4ca953a1d5a9427efb
Rutger Vos <rutgeraldo@gmail.com>
2009-06-02 04:14:01 +0000

    Added createCharacter() and parseSymbol() methods.

--- nexml/java/src/org/nexml/model/Matrix.java
+++ nexml/java/src/org/nexml/model/Matrix.java
@@ -37,6 +37,16 @@ public interface Matrix<T> extends OTUsLinkable {
 	
 	void removeCharacter(Character character);
 	
-	// XXX removeOtu? or removeRow?
+	/**
+	 * This method creates a char element, i.e. a column definition.
+	 * Because NeXML requires for categorical matrices that these
+	 * column definitions have an attribute to reference the 
+	 * applicable state set, the state set object needs to be passed
+	 * in here, from which the attribute's value is set. 
+	 * @author rvosa
+	 */
+	Character createCharacter(CharacterStateSet stateSet);
+	
+	T parseSymbol(String symbol);
 	
 }
