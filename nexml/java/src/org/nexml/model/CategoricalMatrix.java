1b61d42da513001425f2d0e284f782c8a4753c49
Rutger Vos <rutgeraldo@gmail.com>
2009-06-02 04:16:23 +0000

    Moved createCharacter() method up in the inheritance tree.

--- nexml/java/src/org/nexml/model/CategoricalMatrix.java
+++ nexml/java/src/org/nexml/model/CategoricalMatrix.java
@@ -16,16 +16,6 @@ public interface CategoricalMatrix extends Matrix<CharacterState> {
 	 * element).
 	 * @return
 	 */
-	CharacterStateSet createCharacterStateSet();
-	
-	/**
-	 * This method creates a char element, i.e. a column definition.
-	 * Because NeXML requires for categorical matrices that these
-	 * column definitions have an attribute to reference the 
-	 * applicable state set, the state set object needs to be passed
-	 * in here, from which the attribute's value is set. 
-	 * @author rvosa
-	 */	
-	Character createCharacter(CharacterStateSet characterStateSet);
+	CharacterStateSet createCharacterStateSet();	
 	
 }
