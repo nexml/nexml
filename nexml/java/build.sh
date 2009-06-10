6701bab9fcc6a533abc04009e798cef873ff4ca5
Rutger Vos <rutgeraldo@gmail.com>
2009-06-10 07:06:51 +0000

    Added comment to build.sh

--- nexml/java/build.sh
+++ nexml/java/build.sh
@@ -1,3 +1,7 @@
+# this shell script is only needed for updating the nexml.org site - 
+# if you downloaded this from sourceforge or somewhere else and you
+# don't know what to do with it, don't worry: you probably don't need
+# it.
 ant jar
 mv nexml*.jar ../downloads
 ant clean
