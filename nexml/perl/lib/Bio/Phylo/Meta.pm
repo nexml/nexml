7cf4c99e2034e12f74ad44474613866fd6d2131d
Rutger Vos <rutgeraldo@gmail.com>
2009-06-16 08:36:23 +0000

    Adding <meta/> element implementation and xmlliteral value object adaptor. Now passes Pod::Coverage tests.

--- nexml/perl/lib/Bio/Phylo/Meta.pm
+++ nexml/perl/lib/Bio/Phylo/Meta.pm
@@ -7,27 +7,69 @@ use vars qw'@ISA';
 use strict;
 @ISA=qw'Bio::Phylo::Listable';
 
-#Synopsis 
-#
-# use Bio::Phylo::Factory;
-# my $fac = Bio::Phylo::Factory->new;
-#
-# my $proj = $fac->create_project->add_meta(
-#     $fac->create_meta(
-#         '-namespaces' => { 'cdao' => 'http://evolutionaryontology.org#' },
-#         '-triple'     => { 
-#             'cdao:hasMeta' => $fac->create_meta(
-#                 '-namespaces' => { 'cdao' => 'http://evolutionaryontology.org#' },
-#                 '-triple'     => { 'cdao:hasUrl' => 'http://8ball.sdsc.edu:6666/treebase-web/PhyloWS/tree/TreeBASE:2602' }
-#             )
-#         }
-#     )
-# );
-
 {
     my @fields = \( my( %property, %content ) );
     my $TYPE_CONSTANT      = _META_;
     my $CONTAINER_CONSTANT = $TYPE_CONSTANT;
+    
+=head1 NAME
+
+Bio::Phylo::Meta - Single predicate/object annotation, attached to an xml-writable subject
+
+=head1 SYNOPSIS
+
+ use Bio::Phylo::Factory;
+ my $fac = Bio::Phylo::Factory->new;
+ my $base = 'http://8ball.sdsc.edu:6666/treebase-web/PhyloWS/tree/';
+ my $proj = $fac->create_project->add_meta(
+     $fac->create_meta(
+         '-namespaces' => { 'cdao' => 'http://evolutionaryontology.org#' },
+         '-triple'     => { 
+             'cdao:hasMeta' => $fac->create_meta(
+                 '-namespaces' => { 'cdao' => 'http://evolutionaryontology.org#' },
+                 '-triple'     => { 'cdao:has_External_Reference' => $base . 'TreeBASE:2602' }
+             )
+         }
+     )
+ );
+
+=head1 DESCRIPTION
+
+To comply with the NeXML standard (L<http://www.nexml.org>), Bio::Phylo implements
+metadata annotations which consist conceptually of RDF triples where the subject
+is a container object that subclasses of L<Bio::Phylo::Util::XMLWritable>, and the
+predicate and object are defined in this class. 
+
+The objects of the triples provided by this class can be of any simple type (string,
+number) or one of L<XML::DOM>, L<XML::GDOME>, L<XML::LibXML>, L<XML::Twig>, L<XML::DOM2>,
+L<XML::DOMBacked>, L<XML::Handler>, L<XML::Element>, L<XML::API>, L<XML::Code> or
+L<XML::XMLWriter> or L<RDF::Core::Model>.
+
+When serialized, the Bio::Phylo::Meta object in NeXML is typically written out as an element 
+called 'meta', with RDFa compliant attributes.
+
+=head1 METHODS
+
+=head2 CONSTRUCTOR
+
+=over
+
+=item new()
+
+ Type    : Constructor
+ Title   : new
+ Usage   : my $anno = Bio::Phylo::Meta->new;
+ Function: Initializes a Bio::Phylo::Meta object.
+ Returns : A Bio::Phylo::Meta object.
+ Args    : optional constructor arguments are key/value
+ 		   pairs where the key corresponds with any of
+ 		   the methods that starts with set_ (i.e. mutators) 
+ 		   and the value is the permitted argument for such 
+ 		   a method. The method name is changed such that,
+ 		   in order to access the set_value($val) method
+ 		   in the constructor, you would pass -value => $val
+
+=cut    
 
     sub new { return shift->SUPER::new( '-tag' => 'meta', @_ ) }   
     
@@ -87,6 +129,31 @@ use strict;
         }
     };   
     
+=back
+
+=head2 MUTATORS
+
+=over
+
+=item set_triple()
+
+Populates the triple, assuming that the invocant is attached to a subject.
+
+ Type    : Mutator
+ Title   : set_triple
+ Usage   : $meta->set_triple( $predicate, $object );
+ Function: Populates the triple.
+ Returns : Modified object.
+ Args    : $predicate - a CURIE whose namespace prefix must 
+                        have been bound previously using 
+                        $meta->set_namespaces( $prefix, $uri );
+           $object    - any of the valid object types: a number,
+                        a string, a url, a nested annotation
+                        or anything that can be adapted by
+                        Bio::Phylo::Meta::XMLLiteral 
+
+=cut    
+    
     sub set_triple {
         my ( $self, $property, $content ) = @_;
         if ( ref($property) && ref($property) eq 'HASH' ) {
@@ -97,9 +164,72 @@ use strict;
         return $self;
     }    
     
-    sub get_content { $content{ shift->get_id } }    
+=back
+
+=head2 ACCESSORS
+
+=over
+
+=item get_object()
+
+Returns triple object
+
+ Type    : Accessor
+ Title   : get_object
+ Usage   : my $val = $anno->get_object;
+ Function: Returns triple object
+ Returns : A triple object
+ Args    : NONE
+
+=cut    
     
-    sub get_property { $property{ shift->get_id } }
+    sub get_object { $content{ shift->get_id } }    
+    
+=item get_predicate()
+
+Returns triple predicate
+
+ Type    : Accessor
+ Title   : get_predicate
+ Usage   : my $val = $anno->get_predicate;
+ Function: Returns triple predicate
+ Returns : A triple predicate
+ Args    : NONE
+
+=cut     
+    
+    sub get_predicate { $property{ shift->get_id } }
+    
+=back
+
+=cut
+
+# podinherit_insert_token
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Bio::Phylo::Dictionary>
+
+Annotation objects are combined into a dictionary.
+
+=item L<Bio::Phylo::Util::XMLWritable>
+
+This object inherits from L<Bio::Phylo::Util::XMLWritable>, so methods
+defined there are also applicable here.
+
+=item L<Bio::Phylo::Manual>
+
+Also see the manual: L<Bio::Phylo::Manual> and L<http://rutgervos.blogspot.com>.
+
+=back
+
+=head1 REVISION
+
+ $Id: Annotation.pm 1040 2009-05-28 04:26:49Z rvos $
+
+=cut    
     
     sub _type { $TYPE_CONSTANT }
     sub _container { $CONTAINER_CONSTANT }    
@@ -110,61 +240,4 @@ use strict;
 
 }
 
-package Bio::Phylo::Meta::XMLLiteral;
-use Bio::Phylo::Util::CONSTANT qw'_META_';
-use Bio::Phylo::Util::Exceptions 'throw';
-use UNIVERSAL qw'isa can';
-{
-    my $TYPE_CONSTANT      = _META_;
-    my $CONTAINER_CONSTANT = $TYPE_CONSTANT;
-    sub new {
-        my ( $class, $obj ) = @_;
-        return bless \$obj, $class;
-    }
-    sub to_xml {
-        my $self = shift;
-        my $objs = $$self;
-        my @objs = ref($objs) eq 'ARRAY' ? @{ $objs } : ( $objs );
-        my $xml = '';
-		for my $obj ( @objs ) {
-	        # for RDF::Core::Model objects
-	        if ( isa($obj, 'RDF::Core::Model') ) {
-	            eval {
-	                require RDF::Core::Model::Serializer;
-	                my $serialized_model = '';
-	                my $serializer = RDF::Core::Model::Serializer->new(
-	                    'Model'  => $obj,
-	                    'Output' => \$serialized_model,
-	                );   
-	                $xml .= $serialized_model;     			
-	            };
-	            if ( $@ ) {
-	                throw 'API' => $@;
-	            }
-	        }	        
-	        # for XML::XMLWriter object
-	        elsif ( isa($obj, 'XML::XMLWriter') ) {
-	            $xml .= $obj->get;
-	        }	        
-	        else {
-	            # duck-typing
-	            # Bio::Phylo => to_xml, XML::DOM,XML::GDOME,XML::LibXML => toString, XML::Twig => sprint
-	            # XML::DOM2 => xmlify, XML::DOMBacked => as_xml,
-	            # XML::Handler => dump_tree, XML::Element => as_XML
-	            # XML::API => _as_string, XML::Code => code	            
-	            my @methods = qw(to_xml toString sprint _as_string code xmlify as_xml dump_tree as_XML);
-	            SERIALIZER: for my $method ( @methods ) {
-	                if ( can($obj,$method) ) {
-	                    $xml .= $obj->$method;
-	                    last SERIALIZER;
-	                }
-	            }
-	        }
-		}
-        return $xml;        
-    }
-    sub _type { $TYPE_CONSTANT }
-    sub _container { $CONTAINER_CONSTANT }    
-    sub _cleanup {}    
-}
 1;
