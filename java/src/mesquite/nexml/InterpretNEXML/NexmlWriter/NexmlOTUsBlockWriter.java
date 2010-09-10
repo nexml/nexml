7dd956d2d89a45ab353bdbf13be2241bd85c365b
Rutger Vos <rutgeraldo@gmail.com>
2010-09-10 11:43:39 +0000

    Classes that operate on Mesquite objects to write them out as NeXML.

---
+++ java/src/mesquite/nexml/InterpretNEXML/NexmlWriter/NexmlOTUsBlockWriter.java
@@ -0,0 +1,59 @@
+package mesquite.nexml.InterpretNEXML.NexmlWriter;
+
+import mesquite.lib.EmployerEmployee;
+import mesquite.lib.FileElement;
+import mesquite.lib.MesquiteTrunk;
+import mesquite.lib.Taxa;
+import mesquite.nexml.InterpretNEXML.Constants;
+
+import org.nexml.model.Annotatable;
+import org.nexml.model.Document;
+import org.nexml.model.NexmlWritable;
+import org.nexml.model.OTU;
+import org.nexml.model.OTUs;
+
+public class NexmlOTUsBlockWriter extends NexmlBlockWriter {
+
+	/**
+	 * 
+	 * @param employerEmployee
+	 */
+	public NexmlOTUsBlockWriter(EmployerEmployee employerEmployee) {
+		super(employerEmployee);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see mesquite.nexml.InterpretNEXML.NexmlBlockWriter#writeBlock(org.nexml.model.Document, mesquite.lib.FileElement)
+	 */
+	@Override
+	protected Annotatable writeBlock(Document xmlProject, FileElement mesBlock) {
+		OTUs xmlOTUs = xmlProject.createOTUs();
+		Taxa mesTaxa = (Taxa)mesBlock;
+		xmlOTUs.setLabel(mesTaxa.getName());
+		String mesTaxaUID = mesTaxa.getUniqueID();
+		if ( mesTaxaUID == null ) {
+			mesTaxaUID = MesquiteTrunk.getUniqueIDBase() + Taxa.totalCreated;
+			mesTaxa.setUniqueID(mesTaxaUID);
+		}
+		xmlOTUs.addAnnotationValue(Constants.TaxaUID,Constants.BaseURI, mesTaxaUID); // for findEquivalentTaxa
+		for ( int j = 0; j < mesTaxa.getNumTaxa(); j++ ) {			
+			OTU xmlOTU = xmlOTUs.createOTU();
+			xmlOTU.setLabel(mesTaxa.getTaxonName(j));				
+			xmlOTU.addAnnotationValue(Constants.TaxonUID,Constants.BaseURI,new Integer(j)); // for findEquivalentTaxon
+			writeAttributes(mesTaxa.getTaxon(j), xmlOTU);
+		}
+		return xmlOTUs;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see mesquite.nexml.InterpretNEXML.NexmlBlockWriter#getThingInXmlBlock(org.nexml.model.NexmlWritable, int)
+	 */
+	@Override
+	protected Annotatable getThingInXmlBlock(NexmlWritable xmlBlock, int index) {
+		OTUs xmlOTUs = (OTUs)xmlBlock;
+		return xmlOTUs.getAllOTUs().get(index);
+	}
+
+}
