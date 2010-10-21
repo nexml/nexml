994696277a44e15e39c13c940caa366e34ecf78c
Rutger Vos <rutgeraldo@gmail.com>
2010-10-21 15:53:27 +0000

    Going to prepare new minor release, increasing version number

--- perl/lib/Bio/Phylo.pm
+++ perl/lib/Bio/Phylo.pm
@@ -33,7 +33,7 @@ require Bio::Phylo::Mediators::TaxaMediator;
 # Include the revision number from subversion in $VERSION
 my $rev = '$Id$';
 $rev =~ s/^[^\d]+(\d+)\b.*$/$1/;
-$VERSION = "0.21";
+$VERSION = "0.22";
 $VERSION .= "_$rev";
 {
     my $taxamediator = 'Bio::Phylo::Mediators::TaxaMediator';
