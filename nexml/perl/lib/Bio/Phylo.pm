919937bda668a286ab43addf108c3308b35e27ed
Rutger Vos <rutgeraldo@gmail.com>
2010-11-12 23:18:03 +0000

    Incrementing version number for CPAN release

--- nexml/perl/lib/Bio/Phylo.pm
+++ nexml/perl/lib/Bio/Phylo.pm
@@ -33,7 +33,7 @@ require Bio::Phylo::Mediators::TaxaMediator;
 # Include the revision number from subversion in $VERSION
 my $rev = '$Id$';
 $rev =~ s/^[^\d]+(\d+)\b.*$/$1/;
-$VERSION = "0.28";
+$VERSION = "0.29";
 $VERSION .= "_$rev";
 {
     my $taxamediator = 'Bio::Phylo::Mediators::TaxaMediator';
