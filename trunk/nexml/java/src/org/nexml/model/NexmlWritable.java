30067a4ec65b0c02db970dd467fff57ebf08050d
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2011-03-18 13:04:36 +0000

    Added getters/setters for xml:base, setter for ID

--- trunk/nexml/java/src/org/nexml/model/NexmlWritable.java
+++ trunk/nexml/java/src/org/nexml/model/NexmlWritable.java
@@ -1,5 +1,7 @@
 package org.nexml.model;
 
+import java.net.URI;
+
 public interface NexmlWritable {
 	String DEFAULT_NAMESPACE = "http://www.nexml.org/2009";
 	String DEFAULT_VERSION = "0.9";
@@ -29,5 +31,32 @@ public interface NexmlWritable {
 	 * @return
 	 */
 	String getId();
+	
+	/**
+	 * Sets the value of the id attribute. This must be a
+	 * string of type NCName, i.e. a string that matches
+	 * ^[a-zA-Z_][a-zA-Z0-9_\-]*$
+	 * 
+	 *  NOTE: unless you have specific reason to want to
+	 *  change the default auto-generated identifiers
+	 *  there is no need to use this method ever.
+	 * @return
+	 */	
+	void setId(String id);
+
+	/**
+	 * Sets the xml:base attribute for the focal element.
+	 * This facility is used to construct relative URLs 
+	 * other than those computed from the location of the
+	 * document.
+	 * @param baseURI
+	 */
+	void setBaseURI(URI baseURI);
+	
+	/**
+	 * Gets the xml:base attribute.
+	 * @return base URI
+	 */
+	URI getBaseURI();	
 	
 }
