c86c48bdcc69857e453cd06e5effa6b976ff1e5c
rvos <rvos@bbd30ece-093f-0410-93e6-90c30f0fd9a7>
2011-03-30 17:41:17 +0000

    Added conditional to move produced archive to downloads, if it exists. Added podinherit calls.

--- trunk/nexml/perl/build.sh
+++ trunk/nexml/perl/build.sh
@@ -2,9 +2,14 @@
 # other than on the nexml webserver. It builds a CPAN compatible
 # distribution out of the source tree, places that in the downloads
 # folder, then cleans up after itself. That's all.
+DOWNLOADS=../downloads
+./podinherit -dir lib -append
 perl Makefile.PL > /dev/null
 make manifest &> /dev/null
 make dist > /dev/null
 make clean > /dev/null
-mv Bio-Phylo*.tar.gz ../downloads
+if [ -d "$DOWNLOADS" ]; then
+    mv Bio-Phylo*.tar.gz ../downloads
+fi
+./podinherit -dir lib -strip
 rm Makefile.old MANIFEST.bak MANIFEST META.yml
