a6859174fc57142650a62f021f2e170fb97ac42a
Rutger Vos <rutgeraldo@gmail.com>
2011-03-30 18:23:49 +0000

    Added conditionals to ignore podinherit unless env var PODINHERIT has been set

--- nexml/perl/build.sh
+++ nexml/perl/build.sh
@@ -3,7 +3,12 @@
 # distribution out of the source tree, places that in the downloads
 # folder, then cleans up after itself. That's all.
 DOWNLOADS=../downloads
-./podinherit -dir lib -append
+if [ "$PODINHERIT" == "" ]; then
+    echo "Not appending recursive pod because PODINHERIT env var not set"
+fi
+if [ "$PODINHERIT" != "" ]; then
+    ./podinherit -dir lib -append -verbose
+fi
 perl Makefile.PL > /dev/null
 make manifest &> /dev/null
 make dist > /dev/null
@@ -11,5 +16,7 @@ make clean > /dev/null
 if [ -d "$DOWNLOADS" ]; then
     mv Bio-Phylo*.tar.gz ../downloads
 fi
-./podinherit -dir lib -strip
+if [ "$PODINHERIT" != "" ]; then
+    ./podinherit -dir lib -strip -verbose
+fi
 rm Makefile.old MANIFEST.bak MANIFEST META.yml
