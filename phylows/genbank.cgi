d02dab90cce512577cbccfce0227609f8fab4503
rvosa <rutgeraldo@gmail.com>
2014-09-19 14:43:34 +0200

    moved site files to nexml.github.io

--- phylows/genbank.cgi
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use constant URL => 'http://www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=nucleotide&sendto=t&list_uids=';
-require Bio::SeqIO;
-
-my $GI  = '189068371';
-my $url = URL . $GI;
--- site/lib/xs/choice.pm
+++
@@ -1,8 +0,0 @@
-# $Id$
-package xs::choice;
-use strict;
-use warnings;
-use xs::recursiveElementPattern;
-our @ISA=qw(xs::recursiveElementPattern);
-sub explain { 'A choice means that out of the items in the list only one may appear.' }
-1;
